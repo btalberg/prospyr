08067340d26f5c76c458e2e1b31eab557b318f21
Ben Talberg <ben.talberg@gmail.com>
2017-11-02 11:13:10 -0500

    Add ContactType to prospyr package's init

--- prospyr/__init__.py
+++ prospyr/__init__.py
@@ -8,5 +8,5 @@ from prospyr.connection import connect
 from prospyr.resources import (Account, Activity, ActivityType, Company,
                                CustomerSource, Identifier, Lead, LossReason,
                                Opportunity, Person, Pipeline, PipelineStage,
-                               Task, User, Webhook)
+                               Task, User, Webhook, ContactType)
 from prospyr.version import VERSION
